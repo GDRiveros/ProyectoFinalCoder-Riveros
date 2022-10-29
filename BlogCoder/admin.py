d5a4da566419da55e52da52f54f2ca50df2e6829
GDRiveros <gonzalodaniel.r17@gmail.com>
2022-10-29 17:45:44 -0300

    Creacion de modelos, superuser y conexión con el admin

--- BlogCoder/admin.py
+++ BlogCoder/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from BlogCoder.models import Blog, Autor
+
+admin.site.register(Blog)
+admin.site.register(Autor)
--- BlogCoder/models.py
+++ BlogCoder/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    autor = models.CharField(max_length=100)
+    titulo = models.CharField(max_length=100)
+    subtitulo = models.CharField(max_length=100)
+    cuerpo = models.CharField(max_length=100)
+    fecha = models.DateField(null=True)
+    imagen = models.ImageField(upload_to="avatares", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.titulo} de {self.autor}"
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+
+    
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
