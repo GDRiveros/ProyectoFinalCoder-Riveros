115dab2f3ec9287ae5e00395838d5de5fafd6aa5
GDRiveros <gonzalodaniel.r17@gmail.com>
2022-11-04 13:48:36 -0300

    tests

--- BlogCoder/tests.py
+++ BlogCoder/tests.py
@@ -6,24 +6,24 @@ from BlogCoder.models import Autor, Blog
 
 class ViewTestCase(TestCase):
 
-    def primer_test(self):
+    def test_primero(self):
+        autor = Autor.objects.create(nombre="Mario", apellido="Fernandez")
         cantidad_de_autores = Autor.objects.all()
-        assert len(cantidad_de_autores) == 3
+        assert len(cantidad_de_autores) == 1
         
 
-    def segundo_test(self):
-        autores = Autor.objects.all()
-        assert autores[0].nombre == "Oscar"
-        assert autores[0].apellido == "Wilde"
+    def test_segundo(self):
+        autor = Autor.objects.create(nombre="Juan", apellido="Pugliese")
+        autor = Autor.objects.create(nombre="Agustin", apellido="Cisera")
+        todos_los_autores = Autor.objects.all()
+        assert len(todos_los_autores) == 2
+        assert todos_los_autores[0].nombre == "Juan"
+        assert todos_los_autores[1].apellido == "Cisera"
 
 
-    def tercer_test(self):
-        Blog.objects.create(
-            autor="Oscar Wilde",
-            titulo="El Retrato de Dorian Gray",
-            subtitulo="Un joven apasionado",
-            cuerpo="Esta es la historia de...",
-        )
-        cantidad_de_blogs = Blog.objects.all()
-        assert len(cantidad_de_blogs) == 1
+
+    # def test_tercero(self):
+    #     cantidad_de_blogs = Blog.objects.all()
+    #     assert len(cantidad_de_blogs) == 1
+        
 
