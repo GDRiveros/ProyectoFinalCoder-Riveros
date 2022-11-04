a99976408608fd1b2b160fe4add006efc512e049
GDRiveros <gonzalodaniel.r17@gmail.com>
2022-11-04 15:06:24 -0300

    Modificaciones en la app de mensajería

--- Mensajeria/admin.py
+++ Mensajeria/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from Mensajeria.models import Mensaje
+
+
+admin.site.register(Mensaje)
--- Mensajeria/models.py
+++ Mensajeria/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+from django.contrib.auth.models import User
+
+class Mensaje(models.Model):
+    remitente = models.ForeignKey(User, on_delete=models.CASCADE, related_name="remitente")
+    destinatario = models.ForeignKey(User, on_delete=models.CASCADE, related_name="destinatario")
+    mensaje = models.CharField(max_length=100)
+    
